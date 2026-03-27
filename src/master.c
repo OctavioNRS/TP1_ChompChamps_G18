ba9689e160a8f8f66bbb23d59fd2e6685020b0ff
LucasDesmery <ldesmery@itba.edu.ar>
2026-03-27 17:21:24 -0300

    ShareMemory was pushed

--- src/master.c
+++ src/master.c
@@ -60,3 +60,48 @@ int parse_args(int argc, char *argv[], Config *cfg) {
     }
     return 0;
 }
+static size_t gs_size(int w, int h) {
+    return sizeof(GameState) + (size_t)(w * h) * sizeof(char);
+}
+
+static GameState *create_game_state(int width, int height) {
+    size_t total = gs_size(width, height);
+
+    shm_unlink(SHM_STATE);
+    int fd = shm_open(SHM_STATE, O_CREAT | O_RDWR, 0666);
+    if (fd == -1) { perror("master: shm_open game_state"); exit(1); }
+    if (ftruncate(fd, (off_t)total) == -1) { perror("master: ftruncate game_state"); exit(1); }
+
+    GameState *gs = mmap(NULL, total, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (gs == MAP_FAILED) { perror("master: mmap game_state"); exit(1); }
+    close(fd);
+
+    gs->width     = (unsigned short)width;
+    gs->height    = (unsigned short)height;
+    gs->n_players = 0;
+    gs->game_over = false;
+    return gs;
+}
+
+static SyncData *create_sync(int n_players) {
+    shm_unlink(SHM_SYNC);
+    int fd = shm_open(SHM_SYNC, O_CREAT | O_RDWR, 0666);
+    if (fd == -1) { perror("master: shm_open game_sync"); exit(1); }
+    if (ftruncate(fd, (off_t)sizeof(SyncData)) == -1) { perror("master: ftruncate game_sync"); exit(1); }
+
+    SyncData *sd = mmap(NULL, sizeof(SyncData), PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+    if (sd == MAP_FAILED) { perror("master: mmap game_sync"); exit(1); }
+    close(fd);
+
+    sem_init(&sd->view_ready,    1, 0); 
+    sem_init(&sd->view_done,     1, 0); 
+    sem_init(&sd->no_writer,     1, 1); 
+    sem_init(&sd->state_mutex,   1, 1); 
+    sem_init(&sd->readers_mutex, 1, 1); 
+    sd->readers = 0;                    
+
+    for (int i = 0; i < n_players; i++)
+        sem_init(&sd->player_ack[i], 1, 1); 
+
+    return sd;
+}
